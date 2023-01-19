a14a8592eece3d8f4a5ac07cdbf915c9cbd65096
gitrichslow <102004885+gitrichslow@users.noreply.github.com>
2023-01-19 17:44:36 +0000

    looks like it's back up and running

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    return reder_template("index.html")
+    return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -113,8 +113,10 @@ def register():
 
         # Register user if they passed previous tests
         password = request.form.get("password")
+        nickname = request.form.get("password")
 
-        db.execute("INSERT INTO users(username, hash) VALUES(?, ?)", username, generate_password_hash(password))
+        db.execute("INSERT INTO users(username, hash, nickname) VALUES(?, ?, ?)", username, generate_password_hash(password), nickname)
+        return redirect("index.html")
 
     else:
         return render_template("register.html")
