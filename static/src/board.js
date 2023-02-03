ab11f91eee10c9900279783c8e2986437c4761ae
gitrichslow <102004885+gitrichslow@users.noreply.github.com>
2023-02-03 04:10:39 +0000

    added images to the board and removed padding so board is now size of canvas

--- static/src/board.js
+++ static/src/board.js
@@ -1,46 +1,42 @@
 //import './boardUtils.js';
 // Square size
-var ss = 100;
+var ss = 100.0;
 // Board width
-var bw = 500;
+var bw = 501;
 // Board height
-var bh = 500;
+var bh = 501;
 // Padding
 var p = 10;
 
 function drawGrid(ctx) {
     ctx.beginPath();
     for (var x = 0; x <= bw; x += ss) {
-        ctx.moveTo(0.5 + x + p, p);
-        ctx.lineTo(0.5 + x + p, bh + p);
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, bh);
     }
 
     for (var x = 0; x <= bh; x += ss) {
-        ctx.moveTo(p, 0.5 + x + p);
-        ctx.lineTo(bw + p, 0.5 + x + p);
+        ctx.moveTo(0, x);
+        ctx.lineTo(bw, x);
     }
     ctx.strokeStyle = "black";
     ctx.stroke();
-
-    ctx.beginPath();
-    ctx.lineWidth = 15;
-    ctx.strokeStyle = "red";
-    ctx.rect(0.5, 0.5, 520, 520);
-
-
-    ctx.stroke();
 }
 
-function drawPuzzle(ctx, board) {
-    ctx.font = "18px Arial";
 
+function drawPuzzle(ctx, board) {
+    var rocketImage = new Image();
+    rocketImage.src = 'static/images/rocket.png';
+
+    rocketImage.onload = function(){
     board.forEach((row, y) => {
         row.forEach((rocket, x) => {
             if (rocket) {
-                ctx.fillText("*", x*ss+50+p, y*ss+50+p);
+                ctx.drawImage(rocketImage, x*ss+12, y*ss+12, 75, 75);
             }
         })
     })
+    }
 }
 
 function drawBoard() {
