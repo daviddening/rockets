7419758142f7727f13ba835570ff783d248cc8ba
gitrichslow <102004885+gitrichslow@users.noreply.github.com>
2023-03-14 15:51:23 +0000

    added some styling

--- app.py
+++ app.py
@@ -48,17 +48,17 @@ def login():
         # Ensure username was submitted
         if not request.form.get("username"):
             # Info on 403: https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/403
-            return apology("must provide username", 403)
+            return apology("must provide username", 401)
 
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 401)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password", 401)
 
         # Remeber what user logged in
         session["user_id"] = rows[0]["user_id"]
